8a8c6344d2727dd16651fb4ff3aa5bb19d1fe2d0
Harpreet Kaur <148901217+Harpreet8710@users.noreply.github.com>
2025-01-14 21:58:21 -0500

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+import logo from './images/t2.jpg'; 
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+      <img src={logo} alt="Logo" className="logo" />
+
+
+      </div>
+      <div className="navbar-links">
+        <a href="tel:+12340000000" className="navbar-link">+1 (123)-000-0000</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
