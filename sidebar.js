ae8e2e4481b322f8ef5166b1bd61252048969c46
Harpreet Kaur <148901217+Harpreet8710@users.noreply.github.com>
2025-02-07 01:11:58 -0500

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+
+const Sidebar = ({ enrolledCourses }) => {
+  return (
+   <nav className="sidebart">
+          <ul>
+          <li><Link to="/studentdash">List of courses</Link></li>
+            <li>
+              <Link to="/enrolled-courses" state={{ enrolledCourses }}>
+                Enrolled Courses
+              </Link>
+            </li>
+            <li><Link to="/syllabus">Syllabus</Link></li>
+            <li><Link to="/schedule">Course Schedule</Link></li>
+            <li><Link to="/Student-discussion">Discussions</Link></li>
+            <li><Link to="/assignmentsandquizzes">Assignments & Quiz</Link></li>
+            <li><Link to="/graded">Grades</Link></li>
+            <li><Link to="/course-documents">Course Documents</Link></li>
+            <li><Link to="/attendance">Attendance</Link></li>
+            <li><Link to="/notifications">Notifications</Link></li>
+          
+           </ul>
+        </nav>
+  );
+};
+
+export default Sidebar;
